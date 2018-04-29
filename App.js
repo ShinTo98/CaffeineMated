41e2d66696bf0a4db717a9b8de8ce644d7cb2483
Leo (Shih-Chuan) Ku <leo.sc.ku@gmail.com>
2018-04-29 00:34:58 -0700

    restored main page code

--- App.js
+++ App.js
@@ -9,6 +9,15 @@ export default class App extends React.Component {
 
   render() {
     return (
+      // <View style={styles.container}>
+     // <Image
+     //   style={styles.logo}
+     //   source={require('./resources/wei_logo.png')}
+     // />
+     //   <Text style={styles.name}>
+     //     CaffeineMated
+     //   </Text>
+     // </View>
        <RootStack />
     );
   }
