e96dd2481d2dd7501789860126228d1a0e17a558
Dingmei Gu <d3gu@ucsd.edu>
2018-04-30 15:24:17 -0700

    add firebase

--- App.js
+++ App.js
@@ -3,10 +3,26 @@ import {StyleSheet, Text, View, Image} from 'react-native';
 import {LogIn} from './components/LogIn.js';
 import {SignUp} from './components/SignUp.js';
 import {Main} from './components/Main.js';
+import firebase from 'firebase'
 
 import {StackNavigator} from 'react-navigation';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyAQSNocuGrjIBtwErRJeHV7nUsfQGZC_uE",
+      authDomain: "cmdatabase-c3084.firebaseapp.com",
+      databaseURL: "https://cmdatabase-c3084.firebaseio.com",
+      projectId: "cmdatabase-c3084",
+      storageBucket: "cmdatabase-c3084.appspot.com",
+      messagingSenderId: "964208744011"
+    };
+    firebase.initializeApp(config);
+
+  }
 
   render() {
     return (
