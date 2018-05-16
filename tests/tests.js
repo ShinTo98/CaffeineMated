bfe0d08d387ef002a95978e6eaee8b1f664ba77c
Qingchen <qidang@ucsd.edu>
2018-05-15 19:31:16 -0700

    unit tests for signup and login

--- tests/tests.js
+++ tests/tests.js
@@ -1,34 +1,48 @@
-import {userSignup, userLogin, getProfileDetailById, displayMenu, displayType, displayItem, viewPendingOrders, viewOrderDetailById, getOrderLocationById, sortOrders, completeOrder, changeDefaultMode, changeUserName, changeProfilePhoto} from '../database.js';
+import {
+  userSignup,
+  userLogin,
+  getProfileDetailById,
+  displayMenu,
+  displayType,
+  displayItem,
+  viewPendingOrders,
+  viewOrderDetailById,
+  getOrderLocationById,
+  sortOrders,
+  completeOrder,
+  changeDefaultMode,
+  changeUserName,
+  changeProfilePhoto
+} from '../database.js';
 
 //var _Example = require("../database.js\"");
 
 main();
-
 
 function main() {
   testUserSignup();
   testUserLogin();
 }
 
-function testUserSignup( ) {
+async function testUserSignup( ) {
   var email = "unittest@ucsd.edu";
   var password = "password";
-  var returned = userSignup( email, password );
+  var returned = await userSignup( email, password );
 
   console.log( "Testing function userSignup..." );
-  console.log( "Expecting returned value:\t1" );
+  console.log( "Expecting returned value:\t0" );
   console.log( "Actual returned value:\t" + returned );
   if( returned === 0 ) { console.log( "PASSED!\n" ); } else { console.log( "FAILED!\n" ); }
 }
 
 
-function testUserLogin( ) {
+async function testUserLogin( ) {
   var email = "unittest@ucsd.edu";
   var password = "password";
-  var returned = userLogin( email, password );
+  var returned = await userLogin( email, password );
 
   console.log( "Testing function userLogin with correct user credentials..." );
-  console.log( "Expecting returned value:\t1" );
+  console.log( "Expecting returned value:\t0" );
   console.log( "Actual returned value:\t" + returned );
   if( returned === 0 ) { console.log( "PASSED!\n" ); } else { console.log( "FAILED!\n" ); }
 }
