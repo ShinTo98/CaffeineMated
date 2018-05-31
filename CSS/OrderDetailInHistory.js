9e1bcbbefa66bb70671c29b87acae5d828d9519c
Muyuan Chi <muchi@ucsd.edu>
2018-05-30 20:17:22 -0700

    finish basic part of OrderDetailInHistory, need to modify css

---
+++ CSS/OrderDetailInHistory.js
@@ -0,0 +1,28 @@
+import {StyleSheet, Dimensions} from 'react-native';
+
+export const styles = StyleSheet.create({
+    title: {
+        color: '#FF9052',
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: '2%',
+        marginBottom: 0
+    },
+
+    headerContainer: {
+        flex: 1,
+        marginTop: '5%',
+        justifyContent: 'center'
+    },
+
+    userContainer: {
+        flex: 1,
+        height:'10%',
+    },
+
+    orderContainer: {
+        flex: 4,
+        height:'10%',
+    },
+
+});
