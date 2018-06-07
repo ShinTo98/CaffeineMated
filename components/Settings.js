138e5b637b28f62fd65485bd2f73653f5f76d0a5
Leo (Shih-Chuan) Ku <leo.sc.ku@gmail.com>
2018-06-06 20:31:45 -0700

    about link

--- components/Settings.js
+++ components/Settings.js
@@ -154,7 +154,7 @@ export class Settings extends Component {
                   <Icon name="arrow-forward" />
                 </Right>
               </ListItem>
-              <ListItem>
+              <ListItem onPress={() => this.props.navigation.navigate('about')}>
                 <Left>
                   <Text>About</Text>
                 </Left>
